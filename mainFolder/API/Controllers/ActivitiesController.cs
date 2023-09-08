814363946e4f0a5ea8837aa5326947194afa39f8
Milana <milana.zaborovska@gmail.com>
2023-09-08 22:55:30 +0100

    create: new controllers

---
+++ mainFolder/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,13 @@
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+    }
+}
